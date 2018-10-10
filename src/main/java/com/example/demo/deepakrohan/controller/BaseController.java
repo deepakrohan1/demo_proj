6c38c961f2d4322bcc9380cff2d397d68451d824
Deepak Rohan [MacBook Pro] <deepakrohan@hotmail.com>
2018-10-10 13:03:38 -0400

    default route for /

--- src/main/java/com/example/demo/deepakrohan/controller/BaseController.java
+++ src/main/java/com/example/demo/deepakrohan/controller/BaseController.java
@@ -1,5 +1,16 @@
 package com.example.demo.deepakrohan.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
 public class BaseController {
+
+	@RequestMapping("/")
+	public String intialPageOutput() {
+		return "Hello World";
+	}
+	
 
 }
